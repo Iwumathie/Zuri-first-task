59572f64b0390622fd9dfa2961b2f92a8fdf9da1
Iwumathie <iwumatthew@gmail.com>
2023-09-10 14:27:43 -0700

    updated

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 
-const hours = now.getUTCHours();
-const minutes = now.getUTCMinutes();
-const seconds = now.getUTCSeconds();
-const milliseconds = now.getUTCMilliseconds();
+// const hours = now.getUTCHours();
+// const minutes = now.getUTCMinutes();
+// const seconds = now.getUTCSeconds();
+// const milliseconds = now.getUTCMilliseconds();
 
-console.log(`${year}-${month}-${day} 
-            ${hours}:${minutes}:${seconds}.${milliseconds}`);
