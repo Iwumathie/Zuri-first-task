69139a16a295517496277b981473139cd9bb7890
Iwumathie <iwumatthew@gmail.com>
2023-09-11 03:59:15 -0700

    date time display

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 
-// const hours = now.getUTCHours();
-// const minutes = now.getUTCMinutes();
-// const seconds = now.getUTCSeconds();
-// const milliseconds = now.getUTCMilliseconds();
 
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+
+const date = new Date()
+const day = date.getDay()
+const currentDay = weekDays[day]
+
+const dayEl = document.querySelector("h2")
+const timeEl = document.querySelector("p")
+
+dayEl.innerHTML = currentDay
+
+function displayDateTime(){
+
+        const hours = date.getUTCHours();
+        const minutes = date.getUTCMinutes();
+        const seconds =  date.getUTCSeconds(); 
+        const milliseconds = date.getUTCMilliseconds();
+        timeEl.textContent = `${hours}:${minutes}:${seconds}: ${milliseconds}`
+    }
+    
+    window.onload = displayDateTime
+    
+    setInterval(displayDateTime, 1000)
+    
